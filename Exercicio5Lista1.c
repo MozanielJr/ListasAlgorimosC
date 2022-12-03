d5cd90920151292bd87833c39546563087fe0014
MozanielJR <113473875+MozanielJr@users.noreply.github.com>
2022-12-03 12:25:23 -0200

    ecercicio5corrigido

--- Exercicio5Lista1.c
+++ Exercicio5Lista1.c
@@ -1,22 +1,22 @@
-/*
-Fazer um algoritmo para auxiliar um construtor a descobrir a quantidade de rolos e a
+
+/* 5. Fazer um algoritmo para auxiliar um construtor a descobrir a quantidade de rolos e a
 quantidade de metros avulsos de fios que ele precisa adquirir. Sabe-se que o
-construtor tem a quantidade total de fios em metros a serem utilizados na instalação
-elétrica da obra e que cada rolo de fio tem 50 metros.
+construtor tem a quantidade total de fios em metros a serem utilizados na instalaÃ§Ã£o
+elÃ©trica da obra e que cada rolo de fio tem 50 metros.
 */
 
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+main(){
 
+int quantidadeRolo, quantidadeFio, quantidadeTotal;
 
-int quantidadeTotal;
+    printf("Construtor, digite a quantidade total de fios: ");
+        scanf("%d", &quantidadeTotal);
 
-printf("Entao, Construtor, digite a quantidade total de fios que serao utilizados:");
-scanf("%d",quantidadeTotal);
-
-printf("Beleza, voce ira precisar de %d rolos e %d fios avulsos.", quantidadeTotal/50, quantidadeTotal%50);
-
+quantidadeRolo = quantidadeTotal / 50;
+quantidadeFio = quantidadeTotal % 50;
+    printf("serao utilizados %d rolos e %d fios avulsos", quantidadeRolo, quantidadeFio );
 
 
 }
