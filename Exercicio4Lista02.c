ef5b6c57e725e359f420da316c56915b4a3e0cbd
MozanielJR <113473875+MozanielJr@users.noreply.github.com>
2022-12-03 16:21:55 -0200

    ex4

--- Exercicio4Lista02.c
+++ Exercicio4Lista02.c
@@ -2,6 +2,7 @@
 Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma
 de A e B atribuindo o seu resultado na variável X. Imprima o resultado da seguinte
 forma “valor de A” + “valor de B” = “valor de X”.
+
 */
 
 #include<stdio.h>
