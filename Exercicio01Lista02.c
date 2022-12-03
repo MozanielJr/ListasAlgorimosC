356723b5104c01c2634f281f214887be477256f3
MozanielJR <113473875+MozanielJr@users.noreply.github.com>
2022-12-03 15:03:46 -0200

    ex2lt2

---
+++ Exercicio01Lista02.c
@@ -0,0 +1,16 @@
+/*
+O seu primeiro programa em qualquer linguagem de programação normalmente é
+o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir
+esta mensagem na tela.
+*/
+
+#include<stdio.h>
+
+int main(){
+
+
+printf("Hello World! \n");
+
+
+
+}
