54611a972a9f03913ca497d0447c99ea0171bdae
MozanielJR <113473875+MozanielJr@users.noreply.github.com>
2022-12-03 16:12:23 -0200

    ex4

---
+++ Exercicio4Lista02.c
@@ -0,0 +1,27 @@
+/*
+Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma
+de A e B atribuindo o seu resultado na variável X. Imprima o resultado da seguinte
+forma “valor de A” + “valor de B” = “valor de X”.
+*/
+
+#include<stdio.h>
+
+int main(){
+
+int numeroA, numeroB, somaAB;
+
+
+printf("Digite dois numeros inteiros: ");
+scanf("%d%d", &numeroA, &numeroB);
+
+somaAB = numeroA + numeroB;
+
+printf("O Primeiro numero %d + o segundo numero %d = %d \n", numeroA, numeroB, somaAB);
+
+
+
+
+
+
+
+}
